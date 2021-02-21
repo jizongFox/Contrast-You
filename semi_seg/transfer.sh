43e2022ab3b5de03badaaacc4d27e3b596540906
jizong <jizong.peng.1@etsmtl.net>
2021-02-21 12:24:17 +0800

    adding a checkpoint

--- semi_seg/transfer.sh
+++ semi_seg/transfer.sh
@@ -1,8 +1,8 @@
 local_folder=./runs
 
-#rsync -azP  --exclude "*/*.png"  --exclude "*/*.pth" \
-#  beluga:/lustre04/scratch/jizong/Contrast-You/semi_seg/runs/0218/test5 \
-#  "${local_folder}/0218/"
+rsync -azP  --exclude "*/*.png"  --exclude "*/*.pth" \
+  beluga:/lustre04/scratch/jizong/Contrast-You/semi_seg/runs/0219/ \
+  "${local_folder}/0219/"
 
 #rsync -azP  --exclude "*/*.png"  \
 #  shanxi1:/home/jizong/Contrast-You/semi_seg/runs/0218/test5 \
