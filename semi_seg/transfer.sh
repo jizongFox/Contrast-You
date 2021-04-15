21e79a5d0a911e912d45f1e22b093403fc9e9075
jizong <jizong.peng.1@etsmtl.net>
2021-04-15 21:58:59 +0800

    fixing the random seed issue for cpu training

--- semi_seg/transfer.sh
+++ semi_seg/transfer.sh
@@ -10,7 +10,7 @@ local_folder=./runs
 #  "${local_folder}/0402_semi_acdc/"
 
 rsync -azP --exclude "*/*.png"  --exclude "*/tra/*/*.pth" --exclude "*/*.pth" \
-  --exclude "*/patient*"  \
+  --exclude "*/patient*"  --exclude "*/*calculquebec.ca"\
   root@jizong.buzz:/root/main/runs/0416_prostate_semi \
   "${local_folder}"
 
