d83c3879d3a3fa6210667cc24d4e795207c23933
jizong <jizong.peng.1@etsmtl.net>
2021-04-26 02:28:22 +0800

    fix the bug on the mmwhs as the output classes being 5

--- semi_seg/scripts/helper.py
+++ semi_seg/scripts/helper.py
@@ -10,12 +10,12 @@ dataset_name2class_numbers = {
     "acdc": 4,
     "prostate": 2,
     "spleen": 2,
-    "mmwhs": 4,
+    "mmwhs": 5,
 }
 ft_lr_zooms = {"acdc": 0.0000001,
                "prostate": 0.0000005,
                "spleen": 0.000001,
-               "mmwhs": 0.000005}
+               "mmwhs": 0.0000002}
 
 pre_lr_zooms = {"acdc": 0.0000005, "prostate": 0.0000005, "mmwhs": 0.0000005}
 
