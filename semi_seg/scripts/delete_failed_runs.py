4b86af57c4b611cd4e402fc7f783a762c5520a80
jizong <jizong.peng.1@etsmtl.net>
2021-05-07 14:45:02 +0800

    adding script to remove failed experiments

---
+++ semi_seg/scripts/delete_failed_runs.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+from typing import List, TypeVar
+
+from deepclustering2.utils import path2Path
+
+_path_type = TypeVar("_path_type", str, Path)
+
+
+def find_experiment_list(root: _path_type) -> List[Path]:
+    root_: Path = path2Path(root)
+    config_paths = sorted(root_.rglob("*/config.yaml"))
+    experiment_paths = [x.parent for x in config_paths]
+    return experiment_paths
+
+
+def is_experiment_sucessed(path: _path_type):
+    path_: Path = path2Path(path)
+    return (path_ / ".success").exists()
+
+
+def remove_csv(path: _path_type):
+    path_ = path2Path(path)
+    csv_files = list(path_.glob("*.csv"))
+    for c in csv_files:
+        os.remove(c)
+
+
+def main(root: _path_type):
+    root_: Path = path2Path(root)
+    assert root_.is_dir() and root_.exists(), root
+    exp_list = find_experiment_list(root_)
+    print(f"Found {len(exp_list)} experiments.")
+
+    failed_exp_list = [x for x in exp_list if not is_experiment_sucessed(x)]
+    print(f"Found {len(failed_exp_list)} failed experiments.")
+
+    for exp in failed_exp_list:
+        if not is_experiment_sucessed(exp):
+            remove_csv(exp)
+
+
+if __name__ == '__main__':
+    main("/home/jizong/Workspace/Contrast-You/semi_seg/runs/cedar/0505")
