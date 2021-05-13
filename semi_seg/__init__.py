8bbabf5caf48487aa641d5ecff67ada159ba1e8b
jizong <jizong.peng.1@etsmtl.net>
2021-05-14 00:43:46 +0800

    adjust mmwhs dataset with its ratio and learning rate

--- semi_seg/__init__.py
+++ semi_seg/__init__.py
@@ -3,15 +3,15 @@ from contrastyou import on_cc
 acdc_ratios = [0.01, 1.0]
 prostate_ratio = [0.05, 1.0]  # 2 4 8
 prostate_md_ratio = [0.06, 1.0]
-mmwhsct_ratio = [0.09, 1.0]
-mmwhsmr_ratio = [0.10, 1.0]
+mmwhsct_ratio = [0.1, 1.0]
+mmwhsmr_ratio = [0.12, 1.0]
 
 if on_cc():
     acdc_ratios = [0.01, 0.015, 0.025, 1.0]
     prostate_ratio = [0.05, 0.1, 0.2, 1.0]  # 2, 4, 8, 40
-    mmwhsct_ratio = [0.09, 0.17, 0.34, 1.0]
+    mmwhsct_ratio = [0.1, 0.2, 0.4, 1.0]
     prostate_md_ratio = [0.06, 0.12, 0.24, 0.48, 1.0]  # 1, 2, 4, 8
-    mmwhsmr_ratio = [0.09, 0.19, 0.37, 1.0]
+    mmwhsmr_ratio = [0.12, 0.24, 0.45, 1.0]
 
 ratio_zoom = {
     "acdc": acdc_ratios,
@@ -38,20 +38,21 @@ dataset_name2input_dim = {
     "mmwhsmr": 1,
 }
 
-ft_lr_zooms = {
-    "acdc": 0.0000001,
-    "prostate": 0.0000005,
-    "prostate_md": 0.0000005,
-    "spleen": 0.000001,
-    "mmwhsct": 0.000001,
-    "mmwhsmr": 0.000001
-}
 pre_lr_zooms = {
     "acdc": 0.0000005,
     "prostate": 0.0000005,
     "prostate_md": 0.000005,
     "mmwhsct": 0.0000005,
     "mmwhsmr": 0.0000005
+}
+
+ft_lr_zooms = {
+    "acdc": 0.0000001,
+    "prostate": 0.0000005,
+    "prostate_md": 0.0000005,
+    "spleen": 0.000001,
+    "mmwhsct": 0.000002,
+    "mmwhsmr": 0.000002
 }
 __accounts = ["rrg-mpederso", ]
 # __accounts = ["def-chdesa", "rrg-mpederso", "def-mpederso"]
