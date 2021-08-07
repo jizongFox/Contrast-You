5bb09ebc99b14a4fda02c8225d6625c0bb17a1db
jizong <jizong.peng.1@etsmtl.net>
2021-08-07 11:58:04 +0800

    adding scripts on num orders

--- script/script_generator_mt.py
+++ script/script_generator_mt.py
@@ -22,8 +22,9 @@ class MeanTeacherScriptGenerator(BaselineGenerator):
 
         self.hook_config = yaml_load(os.path.join(CONFIG_PATH, "hooks", "mt.yaml"))
 
-    def get_hook_params(self, weight, two_stage, disable_bn):
+    def get_hook_params(self, weight, two_stage, disable_bn, order_num):
         return {
+            "Data": {"order_num": order_num},
             "MeanTeacherParameters":
                 {"weight": weight},
             "Trainer":
@@ -103,7 +104,7 @@ if __name__ == '__main__':
                                                   max_epoch=max_epoch)
 
     jobs = script_generator.grid_search_on(seed=seed, two_stage=[True], disable_bn=False,
-                                           weight=[0.001, 0.01, 0.1, 0.2, 0.5, 1, 5, 10, 20])
+                                           weight=[0.001, 0.01, 0.1, 0.2, 0.5, 1, 5, 10, 20], order_num=[0, 1, 2, 3, 4])
 
     for j in jobs:
         submittor.submit(j, account=next(account), force_show=force_show, time=8)
