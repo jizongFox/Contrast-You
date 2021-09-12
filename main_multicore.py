97c5da7757cbb28479160fdccd0f3405012f3864
jizong <jizong.peng.1@etsmtl.net>
2021-09-13 01:26:40 +0800

    disable deterministric and disable scaled amd

--- main_multicore.py
+++ main_multicore.py
@@ -100,5 +100,6 @@ if __name__ == '__main__':
     import torch
 
     with logger.catch(reraise=True):
-        torch.set_deterministic(True)
+        # torch.set_deterministic(True)
+        torch.backends.cudnn.benchmark = True
         main()
