0b53189b7fa97b1e179786e0b6e35c7843be855d
jizong <jizong.peng.1@etsmtl.net>
2021-11-09 07:34:36 -0500

    adding semi script

--- script/script_generator_pretrain_cc.py
+++ script/script_generator_pretrain_cc.py
@@ -36,6 +36,17 @@ def run_ft(*, save_dir: str, random_seed: int = 10, num_labeled_scan: int, max_e
     """
 
 
+def run_semi(*, save_dir: str, random_seed: int = 10, num_labeled_scan: int, max_epoch: int, num_batches: int,
+             arch_checkpoint: str, lr: float, data_name: str = "acdc", cc_weight: float, mi_weight: float):
+    return f""" python main_nd.py RandomSeed={random_seed} Trainer.name=semi \
+     Trainer.save_dir={save_dir} Trainer.max_epoch={max_epoch} Trainer.num_batches={num_batches} Data.name={data_name} \
+    Data.labeled_scan_num={num_labeled_scan}  Arch.checkpoint={arch_checkpoint} Optim.lr={lr:.10f} \
+    CrossCorrelationParameters.mi_weights={mi_weight}  \
+    CrossCorrelationParameters.cc_weights={cc_weight}  \
+    --path config/base.yaml config/pretrain.yaml config/hooks/ccblocks.yaml 
+    """
+
+
 def run_pretrain_cc(*, save_dir: str, random_seed: int = 10, max_epoch: int, num_batches: int, cc_weight: float,
                     mi_weight: float, lr: float, data_name: str = "acdc"):
     return f"""  python main_nd.py RandomSeed={random_seed} Trainer.name=pretrain_decoder Trainer.save_dir={save_dir} \
@@ -65,6 +76,21 @@ def run_pretrain_ft(*, save_dir, random_seed: int = 10, max_epoch: int, num_batc
         for l in labeled_scans
     ]
     return [pretrain_script] + ft_script
+
+
+def run_semi_regularize(*, save_dir, random_seed: int = 10, max_epoch: int, num_batches: int, data_name: str = "acdc",
+                        mi_weight: float, cc_weight: float) -> List[str]:
+    data_opt = yaml_load(os.path.join(OPT_PATH, data_name + ".yaml"))
+    labeled_scans = data_opt["labeled_ratios"][:-1]
+    semi_script = [
+        run_semi(
+            save_dir=os.path.join(save_dir, "semi", f"labeled_num_{l:03d}"), random_seed=random_seed,
+            num_labeled_scan=l, max_epoch=max_epoch, num_batches=num_batches, arch_checkpoint="null",
+            lr=data_opt["ft_lr"], data_name=data_name, mi_weight=mi_weight,
+            cc_weight=cc_weight)
+        for l in labeled_scans
+    ]
+    return semi_script
 
 
 def run_baseline(
@@ -104,6 +130,27 @@ def run_pretrain_ft_with_grid_search(
                                data_name=data_name)
 
 
+def run_semi_regularize_with_grid_search(
+    *, save_dir, random_seeds: int = 10, max_epoch: int, num_batches: int,
+    data_name: str,
+    mi_weights: Sequence[float], cc_weights: Sequence[float], include_baseline=True
+) -> Iterator[List[str]]:
+    param_generator = grid_search(mi_weight=mi_weights, cc_weight=cc_weights, random_seed=random_seeds)
+    for param in param_generator:
+        random_seed = param.pop("random_seed")
+        sp_str = get_hyper_param_string(**param)
+        yield run_semi_regularize(save_dir=os.path.join(save_dir, f"seed_{random_seed}", sp_str),
+                                  random_seed=random_seed,
+                                  max_epoch=max_epoch, num_batches=num_batches, data_name=data_name, **param)
+
+    if include_baseline:
+        rand_seed_gen = grid_search(random_seed=random_seeds)
+        for random_seed in rand_seed_gen:
+            yield run_baseline(save_dir=os.path.join(save_dir, f"seed_{random_seed['random_seed']}", "baseline"),
+                               **random_seed, max_epoch=max_epoch, num_batches=num_batches,
+                               data_name=data_name)
+
+
 if __name__ == '__main__':
     submitter = SlurmSubmitter(work_dir="../", stop_on_error=False, on_local=on_local)
     submitter.configure_environment([
@@ -128,4 +175,9 @@ if __name__ == '__main__':
     for job in run_pretrain_ft_with_grid_search(save_dir=save_dir, random_seeds=10, max_epoch=50, num_batches=250,
                                                 data_name=data_name, mi_weights=[0.1, 0], cc_weights=[0, 1],
                                                 include_baseline=True):
+        submitter.submit(" && \n ".join(job), force_show=force_show, time=4, account=next(account))
+
+    for job in run_semi_regularize_with_grid_search(save_dir=save_dir, random_seeds=10, max_epoch=50, num_batches=250,
+                                                    data_name=data_name, mi_weights=[0.1, 0], cc_weights=[0, 1],
+                                                    include_baseline=True):
         submitter.submit(" && \n ".join(job), force_show=force_show, time=4, account=next(account))
