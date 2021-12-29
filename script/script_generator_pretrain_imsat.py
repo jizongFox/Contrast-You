f0cc6ea65521f436be7327507e1768d1be122fdf
jizong <jizong.peng.1@etsmtl.net>
2021-12-29 10:59:08 -0500

    adding pretrained_num_scan for imsat

--- script/script_generator_pretrain_imsat.py
+++ script/script_generator_pretrain_imsat.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-from collections.abc import Iterable
 from itertools import cycle
 from typing import Sequence, List, Iterator, Optional
 
@@ -9,48 +8,28 @@ from loguru import logger
 from contrastyou import __accounts, on_cc, MODEL_PATH, OPT_PATH, git_hash
 from contrastyou.configure import yaml_load
 from contrastyou.submitter import SlurmSubmitter
-from script.script_generator_pretrain_cc import _run_ft, _run_ft_per_class
+from script.script_generator_pretrain_cc import _run_ft, _run_ft_per_class, get_hyper_param_string
 from script.utils import grid_search, move_dataset
 
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("save_dir", type=str, help="save dir")
-parser.add_argument("--data-name", type=str, choices=("acdc", "acdc_lv", "acdc_rv", "prostate"), default="acdc",
-                    help="dataset_choice")
-parser.add_argument("--max-epoch-pretrain", default=50, type=int, help="max epoch")
-parser.add_argument("--max-epoch", default=30, type=int, help="max epoch")
-parser.add_argument("--num-batches", default=300, type=int, help="number of batches")
-parser.add_argument("--seeds", type=int, nargs="+", default=[10, ], )
-parser.add_argument("--force-show", action="store_true", help="showing script")
-args = parser.parse_args()
 
-account = cycle(__accounts)
-on_local = not on_cc()
-force_show = args.force_show
-data_name = args.data_name
-random_seeds = args.seeds
-max_epoch = args.max_epoch
-max_epoch_pretrain = args.max_epoch_pretrain
-num_batches = args.num_batches
-
-save_dir = args.save_dir
-
-save_dir = os.path.join(save_dir, f"hash_{git_hash}/{data_name}/imsat")
-
-
-def get_hyper_param_string(**kwargs):
-    def to_str(v):
-        if isinstance(v, Iterable) and (not isinstance(v, str)):
-            return "_".join([str(x) for x in v])
-        return v
-
-    list_string = [f"{k}_{to_str(v)}" for k, v in kwargs.items()]
-    prefix = "/".join(list_string)
-    return prefix
+def get_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("save_dir", type=str, help="save dir")
+    parser.add_argument("--data-name", type=str, choices=("acdc", "acdc_lv", "acdc_rv", "prostate"), default="acdc",
+                        help="dataset_choice")
+    parser.add_argument("--max-epoch-pretrain", default=50, type=int, help="max epoch")
+    parser.add_argument("--max-epoch", default=30, type=int, help="max epoch")
+    parser.add_argument("--num-batches", default=300, type=int, help="number of batches")
+    parser.add_argument("--seeds", type=int, nargs="+", default=[10, ], )
+    parser.add_argument("--pretrain-scan-num", type=int, default=6, help="default `scan_sample_num` for pretraining")
+    parser.add_argument("--force-show", action="store_true", help="showing script")
+    args = parser.parse_args()
+    return args
 
 
 def _run_pretrain_cc(*, save_dir: str, random_seed: int = 10, max_epoch: int, num_batches: int, lr: float,
-                     data_name: str = "acdc", imsat_weight: float, cons_weight: float, num_clusters: int,
-                     num_subheads: int, head_type: str, imsat_lamda: float, use_dynamic: str):
+                     scan_sample_num: int, data_name: str = "acdc", imsat_weight: float, cons_weight: float,
+                     num_clusters: int, num_subheads: int, head_type: str, imsat_lamda: float, use_dynamic: str):
     return f"""  python main_nd.py RandomSeed={random_seed} Trainer.name=pretrain_decoder Trainer.save_dir={save_dir} \
     Trainer.max_epoch={max_epoch} Trainer.num_batches={num_batches}  Optim.lr={lr:.10f} Data.name={data_name} \
     CrossCorrelationParameters.num_clusters={num_clusters}  \
@@ -60,6 +39,7 @@ def _run_pretrain_cc(*, save_dir: str, random_seed: int = 10, max_epoch: int, nu
     CrossCorrelationParameters.hooks.imsat.lamda={imsat_lamda:.10f} \
     CrossCorrelationParameters.hooks.imsat.use_dynamic={use_dynamic} \
     CrossCorrelationParameters.hooks.consist.weight={cons_weight:.10f} \
+    ContrastiveLoaderParams.scan_sample_num={scan_sample_num}  \
     --path config/base.yaml config/pretrain.yaml config/hooks/ccblocks_imsat.yaml \
     """
 
@@ -84,8 +64,8 @@ def _run_semi(*, save_dir: str, random_seed: int = 10, num_labeled_scan: int, ma
 
 
 def run_pretrain_ft(*, save_dir, random_seed: int = 10, max_epoch_pretrain: int, max_epoch: int, num_batches: int,
-                    data_name: str = "acdc", imsat_weight: float, cons_weight: float, num_clusters: int,
-                    num_subheads: int, head_type: str, imsat_lamda: float, use_dynamic: str, ):
+                    pretrain_scan_sample_num: int, data_name: str = "acdc", imsat_weight: float, cons_weight: float,
+                    num_clusters: int, num_subheads: int, head_type: str, imsat_lamda: float, use_dynamic: str, ):
     data_opt = yaml_load(os.path.join(OPT_PATH, data_name + ".yaml"))
     labeled_scans = data_opt["labeled_ratios"][:-1]
     pretrain_save_dir = os.path.join(save_dir, "pretrain")
@@ -93,7 +73,7 @@ def run_pretrain_ft(*, save_dir, random_seed: int = 10, max_epoch_pretrain: int,
         save_dir=pretrain_save_dir, random_seed=random_seed, max_epoch=max_epoch_pretrain, num_batches=num_batches,
         lr=data_opt["pre_lr"], data_name=data_name, imsat_weight=imsat_weight, cons_weight=cons_weight,
         num_clusters=num_clusters, num_subheads=num_subheads, head_type=head_type, imsat_lamda=imsat_lamda,
-        use_dynamic=use_dynamic
+        use_dynamic=use_dynamic, scan_sample_num=pretrain_scan_sample_num
     )
     ft_save_dir = os.path.join(save_dir, "tra")
     if data_name == "acdc":
@@ -157,10 +137,10 @@ def run_pretrain_ft_with_grid_search(
         *, save_dir, random_seeds: Sequence[int] = 10, max_epoch_pretrain: int, max_epoch: int, num_batches: int,
         data_name: str, imsat_weight: Sequence[float], cons_weight: Sequence[float], num_clusters: Sequence[int],
         num_subheads: Sequence[int], head_type: Sequence[str], imsat_lamda: Sequence[float], use_dynamic: Sequence[str],
-        include_baseline=True, max_num: Optional[int] = 200,
+        include_baseline=True, max_num: Optional[int] = 200, pretrain_scan_sample_num: Sequence[int],
 ) -> Iterator[List[str]]:
     param_generator = grid_search(max_num=max_num, imsat_weight=imsat_weight, cons_weight=cons_weight,
-                                  num_clusters=num_clusters,
+                                  num_clusters=num_clusters, pretrain_scan_sample_num=pretrain_scan_sample_num,
                                   num_subheads=num_subheads, head_type=head_type,
                                   imsat_lamda=imsat_lamda,
                                   use_dynamic=use_dynamic,
@@ -209,6 +189,20 @@ def run_semi_regularize_with_grid_search(
 
 
 if __name__ == '__main__':
+    args = get_args()
+    account = cycle(__accounts)
+    on_local = not on_cc()
+    force_show = args.force_show
+    data_name = args.data_name
+    random_seeds = args.seeds
+    max_epoch = args.max_epoch
+    max_epoch_pretrain = args.max_epoch_pretrain
+    num_batches = args.num_batches
+    pretrain_scan_num = args.pretrain_scan_num
+    save_dir = args.save_dir
+
+    save_dir = os.path.join(save_dir, f"hash_{git_hash}/{data_name}/imsat")
+
     submitter = SlurmSubmitter(work_dir="../", stop_on_error=on_local, on_local=on_local)
     submitter.configure_environment([
         # "set -e "
@@ -241,7 +235,8 @@ if __name__ == '__main__':
                                                      head_type=("linear",),
                                                      num_subheads=(3,),
                                                      imsat_lamda=(1, 2, 5, 10),
-                                                     use_dynamic=("false",)
+                                                     use_dynamic=("false",),
+                                                     pretrain_scan_sample_num=(pretrain_scan_num,)
                                                      )
     jobs = list(job_generator)
     logger.info(f"logging {len(jobs)} jobs")
@@ -259,7 +254,8 @@ if __name__ == '__main__':
                                                      head_type=("linear",),
                                                      num_subheads=(3,),
                                                      imsat_lamda=(1,),
-                                                     use_dynamic=("true",)
+                                                     use_dynamic=("true",),
+                                                     pretrain_scan_sample_num=(pretrain_scan_num,)
                                                      )
     jobs = list(job_generator)
     logger.info(f"logging {len(jobs)} jobs")
